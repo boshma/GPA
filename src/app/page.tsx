57cbb01624c40892c81293e9099190f396af6c63
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-05-23 17:35:10 -0700

    db connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       Hello, in progresss
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              {post.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      
+      
+      
+
     </main>
   );
 }
+
