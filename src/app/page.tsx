5dcf3cec08806f282154d563a04d21d20c4d013b
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-05-30 13:46:47 -0700

    main page change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
@@ -11,19 +12,27 @@ export default async function HomePage() {
 
   return (
     <main className="">
-      Hello, in progresss
-      <ul>
-        {images.map((image) => (
-          <li key={image.id}>
-            <Link href={`/posts/${image.id}`}>
-              {image.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      
-      
-      
+      <main className="">
+        <SignedOut>
+          <div className="h-full w-full text-2xl">Please sign in above</div>
+        </SignedOut>
+        <SignedIn>
+          <ul>
+
+            {images.map((image) => (
+              <li key={image.id}>
+                <Link href={`/posts/${image.id}`}>
+                  {image.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+        </SignedIn>
+      </main>
+
+
+
 
     </main>
   );
