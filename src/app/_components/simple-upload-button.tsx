0b3df214c7ce9e0536482b2a0b6394636a945b08
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-06-05 19:11:32 -0700

    add sonner toast component

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
+import { toast } from "sonner";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -30,14 +31,24 @@ const useUploadThingInputProps = (...args: Input) => {
 export function SimpleUploadButton() {
   const router = useRouter();
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading...", {
+        duration: 100000,
+        id: "upload-begin",
+
+      }
+      );
+    },
     onClientUploadComplete() {
+      toast.dismiss("upload-begin");
+      toast("Upload complete")
       router.refresh();
     },
   });
 
   return (
     <div>
-      <label className="cursor-pointer" htmlFor="upload-button"><UploadSvg/></label>
+      <label className="cursor-pointer" htmlFor="upload-button"><UploadSvg /></label>
       <input id="upload-button" type="file" className="sr-only" {...inputProps} />
     </div>
   );
