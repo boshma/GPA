8d76fd9660616de22a4ff567c3880dd5d1069532
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-06-09 07:54:16 -0700

    add foodentries drizzle table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,14 +3,14 @@
 
 import { sql } from "drizzle-orm";
 import {
+  date,
+  doublePrecision,
   index,
   pgTableCreator,
   serial,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
-import { url } from "inspector";
-
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
  * database instance for multiple projects.
@@ -35,5 +35,26 @@ export const images = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
+  })
+);
+
+export const foodEntries = createTable(
+  "food_entry",
+  {
+    id: varchar("id", { length: 256 }).primaryKey().default(sql`gen_random_uuid()`),
+    createdAt: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).default(sql`CURRENT_TIMESTAMP`),
+    name: varchar("name", { length: 256 }).notNull(),
+    protein: doublePrecision("protein").notNull(),
+    carbs: doublePrecision("carbs").notNull(),
+    fat: doublePrecision("fat").notNull(),
+    date: date("date").notNull(),
+    servingSize: doublePrecision("serving_size").default(1).notNull(),
+    userId: varchar("user_id", { length: 256 }).notNull(),
+  },
+  (foodEntry) => ({
+    userIdDateIndex: index("user_id_date_idx").on(foodEntry.userId, foodEntry.date),
   })
 );
