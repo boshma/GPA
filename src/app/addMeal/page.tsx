45eee70388078c3989147ad7f1803f06bc27a104
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-06-09 20:02:42 -0700

    replace add-food-form modal with seperate page

---
+++ src/app/addMeal/page.tsx
@@ -0,0 +1,9 @@
+import { AddMealForm } from "../_meals/add-meal";
+
+export const dynamic = "force-dynamic";
+
+export default function AddMealPage() {
+    return (
+        <AddMealForm />
+    );
+  }
