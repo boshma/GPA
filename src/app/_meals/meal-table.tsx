0eb35eaffe50dfd7e264d0629b9a4a3234a573c4
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-06-14 11:52:07 -0700

    add totals for each macronutrient

--- src/app/_meals/meal-table.tsx
+++ src/app/_meals/meal-table.tsx
@@ -12,58 +12,65 @@ import {
 } from "~/components/ui/table";
 
 export interface Meal {
-    id: string;
-    name: string;
-    protein: number;
-    carbs: number;
-    fat: number;
-    date: string;
-    userId: string;
-  }
+  id: string;
+  name: string;
+  protein: number;
+  carbs: number;
+  fat: number;
+  date: string;
+  userId: string;
+}
 
-  interface MealTableProps {
-    meals: Meal[];
-  }
-  
-  
-  export default function MealTable({ meals }: MealTableProps) {
-    return (
-      <Table>
-        <TableCaption>A list of meals for the selected date.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Meal Name</TableHead>
-            <TableHead>Protein(g)</TableHead>
-            <TableHead>Carbs(g)</TableHead>
-            <TableHead>Fats(g)</TableHead>
-            <TableHead>Calories</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {meals.map((meal) => (
-            <TableRow key={meal.id}>
-              <TableCell className="font-medium">
-                <Link href={`/meal/${meal.id}`}>{meal.name}</Link>
-              </TableCell>
-              <TableCell>{meal.protein}</TableCell>
-              <TableCell>{meal.carbs}</TableCell>
-              <TableCell>{meal.fat}</TableCell>
-              <TableCell>{meal.protein * 4 + meal.carbs * 4 + meal.fat * 9}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={4}>Total</TableCell>
+interface MealTableProps {
+  meals: Meal[];
+}
+
+export default function MealTable({ meals }: MealTableProps) {
+  const totalProtein = meals.reduce((total, meal) => total + meal.protein, 0);
+  const totalCarbs = meals.reduce((total, meal) => total + meal.carbs, 0);
+  const totalFats = meals.reduce((total, meal) => total + meal.fat, 0);
+  const totalCalories = meals.reduce(
+    (total, meal) =>
+      total + (meal.protein * 4 + meal.carbs * 4 + meal.fat * 9),
+    0
+  );
+
+  return (
+    <Table>
+      <TableCaption>A list of meals for the selected date.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Meal Name</TableHead>
+          <TableHead>Protein(g)</TableHead>
+          <TableHead>Carbs(g)</TableHead>
+          <TableHead>Fats(g)</TableHead>
+          <TableHead>Calories</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {meals.map((meal) => (
+          <TableRow key={meal.id}>
+            <TableCell className="font-medium">
+              <Link href={`/meal/${meal.id}`}>{meal.name}</Link>
+            </TableCell>
+            <TableCell>{meal.protein}</TableCell>
+            <TableCell>{meal.carbs}</TableCell>
+            <TableCell>{meal.fat}</TableCell>
             <TableCell>
-              {meals.reduce(
-                (total, meal) =>
-                  total + (meal.protein * 4 + meal.carbs * 4 + meal.fat * 9),
-                0
-              )}
+              {meal.protein * 4 + meal.carbs * 4 + meal.fat * 9}
             </TableCell>
           </TableRow>
-        </TableFooter>
-      </Table>
-    );
-  }
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell>Total</TableCell>
+          <TableCell>{totalProtein}</TableCell>
+          <TableCell>{totalCarbs}</TableCell>
+          <TableCell>{totalFats}</TableCell>
+          <TableCell>{totalCalories}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+}
